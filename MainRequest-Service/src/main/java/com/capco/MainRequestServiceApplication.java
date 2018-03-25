5982be48f2d0c1c784d9b7ec123101daf643fabf
sgkale <shubhamgkale@gmail.com>
2018-03-25 22:56:52 +0530

    first

---
+++ MainRequest-Service/src/main/java/com/capco/MainRequestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.capco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainRequestServiceApplication.class, args);
+	}
+}
