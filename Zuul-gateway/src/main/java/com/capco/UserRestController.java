96ef09c2418e62a8fb0e0a3c86171ce3f56b5b6e
Shubham Kale <Shubham.Kale@capco.com>
2018-03-26 21:57:30 +0530

    added zuul and oauth

---
+++ Zuul-gateway/src/main/java/com/capco/UserRestController.java
@@ -0,0 +1,27 @@
+package com.capco;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/")
+public class UserRestController {
+
+    @RequestMapping(
+            value = "user",
+            method = RequestMethod.GET)
+    public Principal user(Principal user) {
+        return user;
+    }
+    
+    @GetMapping
+	@RequestMapping(value="hello")
+	public String  hello(Principal user) {
+		return "hello"+user.getName();
+		
+	}
+}
